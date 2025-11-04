956e01cf9a983b00b4be85eb4e3f20334cbf24e2
Rael Alexsander <rael.alexsanderf@gmail.com>
2025-11-04 02:50:02 +0000

    retire isso (_for element div_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,22 +1,10 @@
 import Image from "next/image";
-import { PlaceHolderImages } from "@/lib/placeholder-images";
 
 const Header = () => {
-  const logo = PlaceHolderImages.find(p => p.id === 'nail_design_logo');
-
   return (
     <header className="py-4 px-4 sm:px-6 lg:px-8 bg-background/80 backdrop-blur-sm sticky top-0 z-40 border-b">
       <div className="container mx-auto flex justify-center sm:justify-start">
-        {logo && (
-          <Image
-            src={logo.imageUrl}
-            alt={logo.description}
-            width={180}
-            height={45}
-            priority
-            data-ai-hint={logo.imageHint}
-          />
-        )}
+        {/* Logo will be placed here */}
       </div>
     </header>
   );
